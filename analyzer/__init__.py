c2304446d576af3be98baafc881f39719a1ab660
Jacek Kielich <jbackk@gmail.com>
2026-08-14 13:56:54 +0200

    Add files via upload

--- analyzer/__init__.py
+++ analyzer/__init__.py
@@ -2,5 +2,6 @@
 from .adaptive_thresholds import AdaptiveThresholds
 from .timdr_analyzer import TIMDRAnalyzer
 from .synoptyk_v3 import SynoptykV3
+from .synoptyk_v4 import SynoptykV4
 
-__all__ = ["AdaptiveThresholds", "TIMDRAnalyzer", "SynoptykV3"]
+__all__ = ["AdaptiveThresholds", "TIMDRAnalyzer", "SynoptykV3", "SynoptykV4"]
