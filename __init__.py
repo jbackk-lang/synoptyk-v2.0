0de1dad1a33808d10593cff85ec17e6a87f066f3
Jacek Kielich <jbackk@gmail.com>
2026-08-20 03:13:43 +0200

    Delete __init__.py

--- __init__.py
+++
@@ -1,7 +0,0 @@
-# analyzer/__init__.py
-from .adaptive_thresholds import AdaptiveThresholds
-from .timdr_analyzer import TIMDRAnalyzer
-from .synoptyk_v3 import SynoptykV3
-from .synoptyk_v4 import SynoptykV4
-
-__all__ = ["AdaptiveThresholds", "TIMDRAnalyzer", "SynoptykV3", "SynoptykV4"]
